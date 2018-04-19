7fd69b878d386f81f11e1203f1dd7e527ab460e4
Valber Siqueira <valberdevelop@gmail.com>
2018-04-19 11:29:14 -0300

    Atualizaão do webpackconfig

---
+++ src/main/app.jsx
@@ -0,0 +1,14 @@
+import 'modules/bootstrap/dist/css/bootstrap.min.css'
+import 'modules/font-awesome/css/font-awesome.min.css'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import Todo from '../todo/todo'
+
+export default props => (
+    <div className='container'>
+        <Todo/>
+    </div>
+)
+
--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,8 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
     entry: './src/index.jsx',
-    output:{
-        path: __dirname + '/public',    
+    output: {
+        path: __dirname + '/public',
         filename: './app.js'
     },
     devServer: {
@@ -12,29 +12,30 @@ module.exports = {
         contentBase: './public',
     },
     resolve: {
-        extensions: ['', '.js', 'jsx'],
+        extensions: ['', '.js', '.jsx'],
         alias: {
             modules: __dirname + '/node_modules'
         }
     },
-    plugin: [
+    plugins: [
         new ExtractTextPlugin('app.css')
     ],
     module: {
-        loaders:   [{
+        loaders: [{
             test: /.js[x]?$/,
             loader: 'babel-loader',
             exclude: /node_modules/,
             query: {
                 presets: ['es2015', 'react'],
-                plugin: ['transform-object-rest-spread']
+                plugins: ['transform-object-rest-spread']
             }
         },{
             test: /\.css$/,
             loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
         },{
-            test: /\.woff | .woff2 | .ttf | eot | .svg*.*$/,
+            test: /\.woff|.woff2|.ttf|.eot|.svg*.*$/,
             loader: 'file'
         }]
     }
+
 }
